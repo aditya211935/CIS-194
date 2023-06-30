c1c7eb5b8f8f193d431740fc4cc91e96f01d746c
Aditya Sharma <aditya211935@gmail.com>
2023-07-01 03:17:40 +0530

    shorter solutions for week 3

--- week-3/Golf.hs
+++ week-3/Golf.hs
@@ -3,75 +3,43 @@ module Main (main) where
 
 import Data.List
 
-type Curr = Int
-type Step = Int
-type Index = Int
-
-filterNth :: Curr -> Step -> [a] -> [a]
-filterNth _ _ [] = []
-filterNth curr step (x:xs)
-  | curr `mod` step == 0 = x : rest
-  | otherwise = rest
-  where rest = filterNth (curr + 1) step xs
-
-buildSkip :: [a] -> Index -> [[a]]
-buildSkip a i
-  | i <= length a = filterNth 1 i a:buildSkip a (i+1)
-  | otherwise = []
-
--- TODO: make shorter. Probably use list comprehension
 skips :: [a] -> [[a]]
-skips [] = []
-skips a = buildSkip a 1
+skips a = [[fst y | y <- zip a [1..length a], snd y `mod` x == 0] | x <- [1..length a]]
 
+-- Not really shorter than the more straightforward solution
 localMaxima :: [Integer] -> [Integer]
-localMaxima (a:b:c:ds)
-  | b > a && b > c = b : localMaxima (b:c:ds)
-  | otherwise = localMaxima (b:c:ds)
-localMaxima _ = []
+localMaxima = foldr (r . take 3 . reverse) [] . inits
+  where
+    r (a:b:c:_) acc
+      | b > a && b > c = b:acc
+      | otherwise = acc
+    r _ acc = acc
 
-type Count = Integer
-type Value = Integer
-data Elem = Elem Count Value deriving (Show, Eq)
+localMaxima2 :: [Integer] -> [Integer]
+localMaxima2 = map getMid . filter check . filter ((>=3) . length) . map (take 3 . reverse) . inits
+  where getMid (_:b:_) = b
+        getMid _ = 0
+        check (a:b:c:_) = b > a && b > c
+        check _ = False
 
-f :: Integer -> [Elem] -> [Elem]
-f curr [] = [Elem 1 curr]
-f curr (Elem cnt val:xs)
-  | val == curr = (Elem (cnt + 1) val):xs
-  | otherwise = (Elem 1 curr):(Elem cnt val):xs
-
-sc :: [Integer] -> [Elem]
-sc a = foldr f [] (sort a)
-
-row :: [Elem] -> String
-row [] = "\n"
-row (Elem cnt _:xs)
-  | cnt == 0 = " " ++ row xs
-  | otherwise = "*" ++ row xs
-
-sub1 :: [Elem] -> [Elem]
-sub1 [] = []
-sub1 (Elem cnt val: xs)
-  | cnt == 0 = Elem cnt val : sub1 xs
-  | otherwise = Elem (cnt - 1) val : sub1 xs
-
-checkAll :: [Elem] -> Bool
-checkAll [] = False
-checkAll (Elem cnt _:xs) = (cnt > 0) || checkAll xs
-
-cons :: [Elem] -> String
-cons a
-  | (checkAll a) == True = (cons (sub1 a)) ++ (row a)
-  | otherwise = ""
-
-s2 :: [Integer] -> [Elem]
-s2 a = sub1 (sc (a ++ [0,1,2,3,4,5,6,7,8,9]))
-
+-- Short but messy histogram
 histogram :: [Integer] -> String
-histogram a = cons (s2 a) ++ "==========\n0123456789\n"
+histogram a = unlines (foldr (\c acc -> [if x - length acc <= 0 then ' ' else '*' | x <- c]:acc) [] (replicate (maximum cnt) cnt)) ++ "==========\n0123456789\n"
+  where
+    cnt = map (\n -> length n - 1) $ group $ sort $ a ++ [0..9]
+
+-- Takes offset, list of counts and returns a correct row by subtracting offset from list of counts.
+buildRow :: Int -> [Int] -> String
+buildRow off xs = [if x - off <= 0 then ' ' else '*' | x <- xs]
+
+reducerFunc :: [Int] -> [String] -> [String]
+reducerFunc curr acc = buildRow (length acc) curr : acc
+
+-- Longer but a bit cleaner
+histogram2 :: [Integer] -> String
+histogram2 a = unlines (foldr reducerFunc [] (replicate (maximum cnt) cnt)) ++ "==========\n0123456789\n"
+  where cnt = map (\n -> length n - 1) $ group $ sort $ a ++ [0..9]
 
 main :: IO()
 main = do
-  -- let val = (skips []) :: [[Int]]
-  putStr (histogram [1,4,5,4,6,6,3,4,2,4,9])
-  -- putStrLn (row $ s2 [5,4,6,5,4,6,7,6,6])
+  putStr (histogram2 [0..9])
