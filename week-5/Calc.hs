51b5a0357bea9292fff033b348d964f4918e6a81
Aditya Sharma <aditya211935@gmail.com>
2023-07-06 06:08:36 +0530

    week-5: Till exercise 6

--- week-5/Calc.hs
+++ week-5/Calc.hs
@@ -1,9 +1,10 @@
 {-# OPTIONS_GHC -Wall #-}
 -- module LogAnalysis where
 module Main (main) where
-
+import qualified Data.Map as M
 import ExprT
 import Parser
+import qualified StackVM as SVM
 
 eval :: ExprT -> Integer
 eval (Lit a) = a
@@ -45,11 +46,56 @@ instance Expr Mod7 where
   add (Mod7 x) (Mod7 y) = Mod7 ((x + y) `mod` 7)
   mul (Mod7 x) (Mod7 y) = Mod7 ((x * y) `mod` 7)
 
-reify :: Integer -> Integer
-reify = id
+instance Expr SVM.Program where
+  lit x = [SVM.PushI x]
+  add l r = r ++ l ++ [SVM.Add]
+  mul l r = r ++ l ++ [SVM.Mul]
+
+compile :: String -> Maybe SVM.Program
+compile = parseExp lit add mul
+
+run :: Maybe SVM.Program -> Either String SVM.StackVal
+run Nothing = Left "Failed"
+run (Just x) = SVM.stackVM x
+
+class HasVars a where
+  var :: String -> a
+
+instance HasVars VarExprT where
+  var = Var
+
+instance Expr VarExprT where
+  lit = VLit
+  add = VAdd
+  mul = VMul
+
+instance HasVars (M.Map String Integer -> Maybe Integer) where
+  var :: String -> M.Map String Integer -> Maybe Integer
+  var = M.lookup
+
+maybeToTuple :: Maybe a -> Maybe b -> Maybe (a, b)
+maybeToTuple Nothing _ = Nothing
+maybeToTuple _ Nothing = Nothing
+maybeToTuple (Just x) (Just y) = Just (x, y)
+
+instance Expr (M.Map String Integer -> Maybe Integer) where
+  lit x _ = Just x
+  add x y mp = case maybeToTuple (x mp) (y mp) of
+    Nothing -> Nothing
+    Just (xVal, yVal) -> Just (xVal + yVal)
+  mul x y mp = case maybeToTuple (x mp) (y mp) of
+    Nothing -> Nothing
+    Just (xVal, yVal) -> Just (xVal * yVal)
+
+withVars :: [(String, Integer)] -> (M.Map String Integer -> Maybe Integer) -> Maybe Integer
+withVars vs exp2 = exp2 $ M.fromList vs
 
 main :: IO()
 main = do
+  print ((add (lit 3) (var "x")) :: VarExprT)
+  -- print (run $ compile "3+2")
   -- testExp :: Expr a => Maybe a
-  let testExp = parseExp lit add mul "(3 * -4) + 5" :: Maybe Mod7
-  print (testExp)
+  -- let testExp = parseExp lit add mul "(3 * -4) + 5" :: Maybe Mod7
+  -- print (testExp)
+
+
--- week-5/ExprT.hs
+++ week-5/ExprT.hs
@@ -5,5 +5,11 @@ data ExprT = Lit Integer
            | Mul ExprT ExprT
   deriving (Show, Eq)
 
+data VarExprT = VLit Integer
+           | VAdd VarExprT VarExprT
+           | VMul VarExprT VarExprT
+           | Var String
+  deriving (Show, Eq)
+
 newtype MinMax = MinMax Integer deriving (Eq, Show)
 newtype Mod7 = Mod7 Integer deriving (Eq, Show)
